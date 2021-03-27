9a9622adb978c1c951ea8ecd05a444c3d3954a3e
Abdullah Waseem <80362352+abdullahwaseem01@users.noreply.github.com>
2021-03-27 16:24:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -43,10 +43,20 @@ document
 });
 
 
+window.onload = getLocation();
 
-  
+function getLocation() {
+  if (navigator.geolocation) {navigator.geolocation.getCurrentPosition(showPosition);} 
+    else { console.log("Geolocation is not supported by this browser.");}
+}
 
-  
+function showPosition(position) {
+    console.log(position.coords.latitude, position.coords.longitude);
+};
+ 
+
+const cname = "oshawa"
+weather.fetchWeather(cname)
 
 
-  
+
